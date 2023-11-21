742b3549ab757f13b6cdf380d95a8c36dc0a6611
GHERGUT MADALIN <madalinghergut@gmail.com>
2023-11-21 19:45:08 +0200

    Basic Circuit Breaker , beautiful and easy

--- microservices-new/order-service/src/main/java/app/controller/OrderController.java
+++ microservices-new/order-service/src/main/java/app/controller/OrderController.java
@@ -2,6 +2,7 @@ package app.controller;
 
 import app.dto.OrderRequest;
 import app.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +16,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String createOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order is placed successfully !";
     }
-
+//    Needs to have the same signature as the method it is replacing
+    public String fallbackMethod(OrderRequest orderRequest , RuntimeException e){
+        return "Order Service is taking too long to respond or is down. Please try again later";
+    }
 }
