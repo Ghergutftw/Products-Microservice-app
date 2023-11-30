b2e12b7fdac783d664692e0a37af9bebaac1a94c
GherMa <89106572+Ghergutftw@users.noreply.github.com>
2023-12-01 01:23:47 +0200

    Delete order-service directory

--- order-service/src/main/java/app/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package app.repository;
-
-import app.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
