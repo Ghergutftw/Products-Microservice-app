e8ab01f193e2fac497a9f7e72d20b3adabca9e9c
GHERGUT MADALIN <madalinghergut@gmail.com>
2023-12-01 01:05:42 +0200

    Adding Kafka + Dockerized Partially <3

--- microservices-new/order-service/src/main/java/app/service/OrderService.java
+++ microservices-new/order-service/src/main/java/app/service/OrderService.java
@@ -3,18 +3,21 @@ package app.service;
 import app.dto.InventoryResponse;
 import app.dto.OrderLineItemsDto;
 import app.dto.OrderRequest;
+import app.event.OrderPlacedEvent;
 import app.model.Order;
 import app.model.OrderLineItems;
 import app.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+
 
 @Service
 @Slf4j
@@ -24,6 +27,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -48,6 +52,7 @@ public class OrderService {
 
         if (allProductsInStock) {
             orderRepository.save(order);
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
             return "Order is placed successfully !";
         } else {
             throw new IllegalArgumentException("Order failed due to product(s) not in stock");
